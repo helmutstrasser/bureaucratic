93c50a191282276ba72a8581c796bbd37bbdb9d6
Josef Glatz <typo3@josefglatz.at>
2022-09-27 22:20:21 +0200

    [FEATURE] be_users: make email, realName required fields

---
+++ Configuration/TCA/Overrides/be_users.php
@@ -0,0 +1,28 @@
+<?php
+defined('TYPO3_MODE') || die('Access denied.');
+
+call_user_func(
+    function ($extKey, $table) {
+        $tca = [
+            'columns' => [
+                'realName' => [
+                    'config' => [
+                        'eval' => 'trim,required',
+                    ],
+                ],
+                'email' => [
+                    'config' => [
+                        'eval' => 'trim,email,required',
+                    ],
+                ],
+            ],
+        ];
+        $GLOBALS['TCA'][$table] = array_replace_recursive($GLOBALS['TCA'][$table], $tca);
+
+        $additionalColumns = [];
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns($table, $additionalColumns);
+
+    },
+    'bureaucratic',
+    'be_users'
+);
---
+++ README.md
@@ -0,0 +1,8 @@
+# josefglatz/bureaucratic – basics for professional TYPO3 projects
+
+# Backend
+
+## Required fields for backend users
+
+* realName
+* email
