967bfa52fe160b533a2cc25b0c8c9ed708e5cec0
Josef Glatz <j.glatz@supseven.at>
2023-09-06 07:03:58 +0200

    [TASK] Raise TYPO3 version constraints
    Fixes: #2

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,7 +5,7 @@ $EM_CONF[$_EXTKEY] = [
     'description' => 'Basics for professional TYPO3 projects',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-11.5.99',
+            'typo3' => '11.5.0-12.4.99',
         ],
     ],
     'version' => '1.0.0',
