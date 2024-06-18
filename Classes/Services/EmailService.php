7c18c9fb7a12e95b947809d62c39e971ada67fa4
Josef Glatz <j.glatz@supseven.at>
2024-06-18 20:53:50 +0200

    [TASK] Add empty EmailService

---
+++ Classes/Services/EmailService.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace JosefGlatz\Bureaucratic\Services;
+
+/**
+ * Class EmailService
+ * @author Josef Glatz <j.glatz@supseven.at>
+ */
+class EmailService
+{
+
+}
